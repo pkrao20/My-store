827c578dde2fdb8d24a921c04a2eae9c4c8d960e
Prashant Rao <pkraojee2020@gmail.com>
2025-08-27 00:26:03 +0530

    added increment and decrement and changed categories

--- constant/index.tsx
+++ constant/index.tsx
@@ -1,1 +1,2 @@
-export  const categories = ["Electronics", "Fashion", "Home & Kitchen", "Beauty", "Sports", "Toys"];
+// export  const categories = ["Electronics", "Fashion", "Home & Kitchen", "Beauty", "Sports", "Toys"];export const categories = ["men's clothing", "women's clothing", "jewelery", "electronics"];
+export const categories = ["men's clothing", "women's clothing", "jewelery", "electronics"];
